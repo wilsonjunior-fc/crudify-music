a1e3256bfb0eebc9a7065a46682c8e53ede022c8
Linksyyy <enzovidero@gmail.com>
2025-08-30 00:39:29 -0300

    Added load screen on init and a few delay to load to look at my pretty 3D cube

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+setTimeout(() => {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}, 500
+)
